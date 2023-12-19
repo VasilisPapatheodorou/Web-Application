d8005c5a4b403a52b56c4d4b904e1ee0b3eb6f02
Vasilis Papatheodorou <billy.papatheo@gmail.com>
2023-12-19 13:08:51 +0200

    Added form on category.html

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const uuidv4 = require('uuid').v4;
 
 const app = express();
 
@@ -18,6 +20,20 @@ app.get('/category',(req,res) => {
 app.get('/subcategory',(req,res) => {
   res.sendFile(path.join(__dirname, 'subcategory.html'))
 })
+
+app.use(bodyParser.json());
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  // Dummy authentication logic (replace with your actual authentication logic)
+  if (username === 'demo' && password === 'demo') {
+    const sessionId = uuidv4();
+    res.json({ username,sessionId });
+  } else {
+    res.status(401).json({ error: 'Invalid credentials' });
+  }
+});
 
 
 // Start the server and listen on port 3000
